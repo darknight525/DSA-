6259f99e4a695ab2b154fed3e970ce3c14eef687
NITIN SINGH <nitin072005singh@gmail.com>
2026-02-14 10:24:58 +0530

    Time: 15 ms (76.38%) | Memory: 24.6 MB (85.79%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        mp = {}
+
+        for x in nums:
+            if x in mp:
+                return True
+            mp[x] = 1
+
+        return False
+
+
+
+        
+        
